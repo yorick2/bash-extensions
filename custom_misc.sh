299e4722fa561e2ce1854db7697f4eab14320009
Paul Millband <millband_paul@hotmail.co.uk>
2016-11-02 13:35:51 +0000

    list custom commands

--- custom_misc.sh
+++ custom_misc.sh
@@ -5,32 +5,34 @@ alias n98re='echo running n98-magerun.phar index:reindex:all; n98-magerun.phar i
 alias n98dis='echo running n98-magerun.phar cache:disable; n98-magerun.phar cache:disable'
 alias rmcache='echo "rm -rf var/cache/* var/session/*"; rm -rf var/cache/* var/session/*'
 
-alias phpstorm='/Applications/PhpStorm.app/Contents/MacOS/phpstorm'
-alias phpstorm_diff='/Applications/PhpStorm.app/Contents/MacOS/phpstorm diff'
+function open(){
+	nautilus ${1} 
+}
 
 function listCustomCommands(){
+  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
   if [ "$1" = "git"  ] ; then
-        grep function ~/.oh-my-zsh/custom/gitextension.zsh | grep -v 'grep' | sed -e's/\s*function\s*//' | cut -f1 -d"(" ;
-        grep alias ~/.oh-my-zsh/custom/gitextension.zsh | grep -v 'grep' | sed -e's/\s*alias\s*//' | cut -f1 -d"=" ;
+        grep function ${DIR}/gitextension.sh | grep -v 'grep' | sed -e's/\s*function\s*//' | cut -f1 -d"(" ;
+        grep alias ${DIR}/gitextension.sh | grep -v 'grep' | sed -e's/\s*alias\s*//' | cut -f1 -d"=" ;
   elif [ "$1" = "local"  ] ; then
-        grep function ~/.oh-my-zsh/custom/local_setup.zsh | grep -v 'grep' | sed -e's/\s*function\s*//' | cut -f1 -d"(" ;
-        grep alias ~/.oh-my-zsh/custom/local_setup.zsh | grep -v 'grep' | sed -e's/\s*alias\s*//' | cut -f1 -d"=" ;
+        grep function ${DIR}/local_setup.sh | grep -v 'grep' | sed -e's/\s*function\s*//' | cut -f1 -d"(" ;
+        grep alias ${DIR}/local_setup.sh | grep -v 'grep' | sed -e's/\s*alias\s*//' | cut -f1 -d"=" ;
   elif [ "$1" = "custom"  ] ; then
-        grep function ~/.oh-my-zsh/custom/custom_misc.zsh | grep -v 'grep' | sed -e's/\s*function\s*//' | cut -f1 -d"(" ;
-        grep alias ~/.oh-my-zsh/custom/custom_misc.zsh | grep -v 'grep' | sed -e's/\s*alias\s*//' | cut -f1 -d"=" ;
+        grep function ${DIR}/custom_misc.sh | grep -v 'grep' | sed -e's/\s*function\s*//' | cut -f1 -d"(" ;
+        grep alias ${DIR}/custom_misc.sh | grep -v 'grep' | sed -e's/\s*alias\s*//' | cut -f1 -d"=" ;
   elif [ "$1" = "personal"  ] ; then
-        grep function ~/.oh-my-zsh/custom/personal.zsh | grep -v 'grep' | sed -e's/\s*function\s*//' | cut -f1 -d"(" ;
-        grep alias ~/.oh-my-zsh/custom/personal.zsh | grep -v 'grep' | sed -e's/\s*alias\s*//' | cut -f1 -d"=" ;
+        grep function ${DIR}/personal.sh | grep -v 'grep' | sed -e's/\s*function\s*//' | cut -f1 -d"(" ;
+        grep alias ${DIR}/personal.sh | grep -v 'grep' | sed -e's/\s*alias\s*//' | cut -f1 -d"=" ;
   else
          { \
-          grep function ~/.oh-my-zsh/custom/gitextension.zsh | grep -v 'grep' | sed -e's/\s*function\s*//' | cut -f1 -d"(" ; \
-          grep function ~/.oh-my-zsh/custom/local_setup.zsh | grep -v 'grep' | sed -e's/\s*function\s*//' | cut -f1 -d"(" ; \
-          grep function ~/.oh-my-zsh/custom/custom_misc.zsh | grep -v 'grep' | sed -e's/\s*function\s*//' | cut -f1 -d"(" ; \
-          grep function ~/.oh-my-zsh/custom/personal.zsh | grep -v 'grep' | sed -e's/\s*function\s*//' | cut -f1 -d"(" ; \
-          grep alias ~/.oh-my-zsh/custom/gitextension.zsh | grep -v 'grep' | sed -e's/\s*alias\s*//' | cut -f1 -d"=" ; \
-          grep alias ~/.oh-my-zsh/custom/local_setup.zsh | grep -v 'grep' | sed -e's/\s*alias\s*//' | cut -f1 -d"=" ; \
-          grep alias ~/.oh-my-zsh/custom/custom_misc.zsh | grep -v 'grep' | sed -e's/\s*alias\s*//' | cut -f1 -d"=" ; \
-          grep alias ~/.oh-my-zsh/custom/personal.zsh | grep -v 'grep' | sed -e's/\s*alias\s*//' | cut -f1 -d"=" ; \
+          grep function ${DIR}/gitextension.sh | grep -v 'grep' | sed -e's/\s*function\s*//' | cut -f1 -d"(" ; \
+          grep function ${DIR}/local_setup.sh | grep -v 'grep' | sed -e's/\s*function\s*//' | cut -f1 -d"(" ; \
+          grep function ${DIR}/custom_misc.sh | grep -v 'grep' | sed -e's/\s*function\s*//' | cut -f1 -d"(" ; \
+          grep function ${DIR}/personal.sh | grep -v 'grep' | sed -e's/\s*function\s*//' | cut -f1 -d"(" ; \
+          grep alias ${DIR}/gitextension.sh | grep -v 'grep' | sed -e's/\s*alias\s*//' | cut -f1 -d"=" ; \
+          grep alias ${DIR}/local_setup.sh | grep -v 'grep' | sed -e's/\s*alias\s*//' | cut -f1 -d"=" ; \
+          grep alias ${DIR}/custom_misc.sh | grep -v 'grep' | sed -e's/\s*alias\s*//' | cut -f1 -d"=" ; \
+          grep alias ${DIR}/personal.sh | grep -v 'grep' | sed -e's/\s*alias\s*//' | cut -f1 -d"=" ; \
         } | grep "${1}";
   fi 
 }
