a6520afc2875d6d3db262e371090ca0e2ff3bfd0
Paul Millband <millband_paul@hotmail.co.uk>
2016-11-03 12:25:45 +0000

    n98 2

--- magento.sh
+++ magento.sh
@@ -12,6 +12,13 @@ alias n98dis='echo running n98-magerun.phar cache:disable; n98-magerun.phar cach
 alias rmcache='echo "rm -rf var/cache/* var/session/*"; rm -rf var/cache/* var/session/*'
 
 # magento 2
+alias n982='echo running n98-magerun2.phar; n98-magerun2.phar'
+alias n982fl='echo running n98-magerun2.phar cache:flush; n98-magerun2.phar cache:flush'
+alias n982nu='echo running n98-magerun2.phar admin:user:create; n98-magerun2.phar admin:user:create'
+alias n982pass='echo running n98-magerun2.phar admin:user:change-password; n98-magerun2.phar admin:user:change-password'
+alias n982re='echo running n98-magerun2.phar index:reindex:all; n98-magerun2.phar index:reindex:all'
+alias n982dis='echo running n98-magerun2.phar cache:disable; n98-magerun2.phar cache:disable'
+
 alias magento2UpgradeNStatic="echo 'php bin/magento setup:upgrade \
  && php bin/magento setup:static-content:deploy \
  && php bin/magento cache:clean' \
