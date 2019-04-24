0deaa3d8998b68fff65aa1980d0de4d867c0f208
yorick <millband_paul@hotmail.co.uk>
2019-04-24 11:03:36 +0100

    dockerssh auto complete options list load time issue

--- bash-completion/docker.sh
+++ bash-completion/docker.sh
@@ -1,2 +1,7 @@
-complete -W "$(docker ps -q) $(docker ps --format '{{.Names}}')"  dockerssh
-
+_dockerssh(){
+    # do not change to single line "complete -W ..." as this is saved as static set of options, so as this is a dynamic
+    # list it isnt suitable
+    _fromArrayAutoCompletion "$(docker ps -q) $(docker ps --format '{{.Names}}')";
+}
+#Assign the auto-completion function _get for our command get.
+complete -F _dockerssh dockerssh
