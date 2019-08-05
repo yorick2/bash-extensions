1d7a7dbe05552d0e65de6dd6f5ec56d97be499f4
Paul Millband <millband_paul@hotmail.co.uk>
2019-08-05 14:37:07 +0100

    dockerDestroyAllContainersAndImages

--- docker.sh
+++ docker.sh
@@ -13,5 +13,30 @@ function dockerssh(){
     echo ;
     docker exec -it ${1} /bin/bash;
 }
+function dockerDestroyAllContainersAndImages(){
+    if [ "$1" = "-help" ]
+    then
+        echo "destroy all containers and images completely"
+        return
+    fi
+    destroyContainers=''
+    while [[ "${destroyContainers}" != "y" && "${destroyContainers}" != "n" ]] ; do
+        echo 'Destroy all containers? [y/n]';
+        read destroyContainers;
+    done;
+    destroyImages=''
+    while [[ "${destroyImages}" != "y" && "${destroyImages}" != "n" ]] ; do
+        echo 'Destroy all images? [y/n]';
+        read destroyImages;
+    done;
+    if [[ ${destroyContainers} == "y" ]] ; then
+        containers=$(sudo docker ps -a -q);
+        if [ -n "$containers" ]; then sudo docker rm -f -v $containers; fi;
+    fi
+    if [[ ${destroyImages} == "y" ]] ; then
+        images=$(sudo docker images -q -a);
+        if [ -n "$images" ]; then sudo docker rmi -f $images; fi;
+    fi
+}
 
 
