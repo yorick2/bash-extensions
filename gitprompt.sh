610eb2e9739e882538c6e0377518ec8a01cd0701
yorick <millband_paul@hotmail.co.uk>
2020-04-29 23:03:50 +0100

    toggle git prompt

--- gitprompt.sh
+++ gitprompt.sh
@@ -39,19 +39,33 @@ function branch_data() {
     fi
 }
 
-if [ -n "$force_git_prompt" ]; then
-    if [ -n "$force_git_color_prompt" ]; then
-        if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
-            # We have color support; assume it's compliant with Ecma-48
-            # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
-            # a case would tend to support setf rather than setaf.)
-            color_prompt=yes
-            customBashPrompt='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;31m\]$(branch_data)\[\033[00m\]\$ '
-            export PS1="[$(date +%H:%M:%S)] ${customBashPrompt}"; # add time to line
-            return 1
-        fi
-    fi
-    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w$(branch_data)\$ '
-    export PS1="[$(date +%H:%M:%S)] ${PS1}" # add time to line
-    unset color_prompt
-fi
+
+PS1_original="$PS1"
+function reloadGitPrompt(){
+	if [ -n "$force_git_prompt" ]; then
+	    if [ -n "$force_git_color_prompt" ]; then
+	        if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+	            # We have color support; assume it's compliant with Ecma-48
+	            # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+	            # a case would tend to support setf rather than setaf.)
+	            color_prompt=yes
+	            customBashPrompt='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;31m\]$(branch_data)\[\033[00m\]\$ '
+	            export PS1="[$(date +%H:%M:%S)] ${customBashPrompt}"; # add time to line
+	            return 1
+	        fi
+	    fi
+	    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w$(branch_data)\$ '
+	    export PS1="[$(date +%H:%M:%S)] ${PS1}" # add time to line
+	    unset color_prompt
+	fi
+	export PS1="$PS1_original"
+}
+function enableGitPrompt(){
+	force_git_prompt=true
+	reloadGitPrompt
+}
+function disableGitPrompt(){
+	unset force_git_prompt
+	reloadGitPrompt
+}
+reloadGitPrompt
