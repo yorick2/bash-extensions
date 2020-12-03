be1895d7b03670f11954529397c0a7b1b8c93192
Paul Millband <millband_paul@hotmail.co.uk>
2020-12-03 15:16:04 +0000

    stop error when git autocomplete not installed

--- bash-completion/gitextension.sh
+++ bash-completion/gitextension.sh
@@ -6,22 +6,24 @@ if [ -z "$(type -t __git_complete)" ] && [ -f ~/.git-completion.bash ]; then
   . ~/.git-completion.bash;
 fi;
 
-# git autocompletes
-__git_complete g __git_main
-__git_complete ga _git_add
-__git_complete gc _git_commit
-__git_complete gco _git_checkout
-__git_complete gcp _git_cherry_pick
-__git_complete gb _git_branch
-__git_complete gst _git_status
-__git_complete gl _git_pull
-__git_complete gm _git_merge
-__git_complete gm2b _git_merge
-__git_complete gmm _git_merge
-__git_complete gmd _git_merge
-__git_complete gmb2b _git_merge
-__git_complete grup _git_remotes
+# if __git_complete  commandexists
+if [ "$(type -t __git_complete)" ]; then 
+	# git autocompletes
+	__git_complete g __git_main
+	__git_complete ga _git_add
+	__git_complete gc _git_commit
+	__git_complete gco _git_checkout
+	__git_complete gcp _git_cherry_pick
+	__git_complete gb _git_branch
+	__git_complete gst _git_status
+	__git_complete gl _git_pull
+	__git_complete gm _git_merge
+	__git_complete gm2b _git_merge
+	__git_complete gmm _git_merge
+	__git_complete gmd _git_merge
+	__git_complete gmb2b _git_merge
+	__git_complete grup _git_remotes
 
-__git_complete git_merge_branchs _git_checkout
-__git_complete gb2b _git_checkout
-
+	__git_complete git_merge_branchs _git_checkout
+	__git_complete gb2b _git_checkout
+fi
